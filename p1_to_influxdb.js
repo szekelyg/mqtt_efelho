924dd7817473b3f15997d5a9290baef7be0f45a5
szekelyg <36407132+szekelyg@users.noreply.github.com>
2023-10-17 10:02:55 +0200

    Update p1_to_influxdb.js

--- p1_to_influxdb.js
+++ p1_to_influxdb.js
@@ -2,14 +2,14 @@ const mqtt = require('mqtt');
 const { InfluxDB, Point } = require('@influxdata/influxdb-client');
 
 const influxdb_url = 'https://eu-central-1-1.aws.cloud2.influxdata.com';
-const token = 'lhfnQvKJn7ZO9P_GEnyCy-sXMfy3y7eYxwuC3aj0jDQeOUtMwvc-7vYfQNsc69NH4wDtoCosrpv-R8ndHQezjA==';
-const org = 'Energiafelho';
+const token = '7VfH77yCJenqjk-zOT24Bz-nMFrePCAT6Lez8NNUIhO7DB0HOzsWxgXnvgmELe-4s02NANjprUMqSC_kX0bORA==';
+const org = 'b08ba55f6db726a3';
 const bucket = 'efelho';
 
 const client = new InfluxDB({ url: influxdb_url, token: token });
 const writeApi = client.getWriteApi(org, bucket);
 
-const mqttClient = mqtt.connect('mqtt://722577b8ac4a4176ac5460ef90db0940.s2.eu.hivemq.cloud');
+const mqttClient = mqtt.connect('mqtt://722577b8ac4a4176ac5460ef90db0940.s2.eu.hivemq.cloud',"szekelyg","Sevenof9");
 
 mqttClient.on('connect', () => {
   mqttClient.subscribe('SmartMeter/P1', (err) => {
