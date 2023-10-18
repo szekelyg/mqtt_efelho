ea7be246f7a7195223feb81a3648fc81a7e383dd
szekelyg <szekely.gellerth@gmail.com>
2023-10-18 16:47:23 +0200

    html2.0

--- public/script.js
+++ public/script.js
@@ -1,60 +1,55 @@
-window.onload = function() {
-    fetchDevices();
-};
+// public/script.js
+let selectedDevice = null;
 
+// Fetch the list of connected devices and update the UI
 function fetchDevices() {
-    fetch('/api/devices')
-    .then(response => response.json())
-    .then(devices => {
-        const deviceList = document.getElementById('deviceList');
-        deviceList.innerHTML = '';
-        devices.forEach(device => {
-            const option = document.createElement('option');
-            option.value = device;
-            option.textContent = device;
-            deviceList.appendChild(option);
+    fetch('/devices/status')
+        .then(response => response.json())
+        .then(data => {
+            const deviceList = document.getElementById('deviceList');
+            deviceList.innerHTML = '';
+            for (const device in data) {
+                if (data[device].status === 'online') {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = device;
+                    listItem.onclick = function() {
+                        selectedDevice = device;
+                        listItem.style.backgroundColor = 'lightblue';
+                    };
+                    deviceList.appendChild(listItem);
+                }
+            }
         });
-    });
 }
 
+// Send a command to the selected device
 function sendCommand() {
-    const device = document.getElementById('deviceList').value;
-    const command = document.getElementById('commandInput').value;
-
-    fetch('/api/send-command', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            device: device,
-            command: command
+    const commandInput = document.getElementById('commandInput');
+    const message = document.getElementById('message');
+    if (selectedDevice) {
+        fetch('/api/send-command', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                device: selectedDevice,
+                command: commandInput.value
+            })
         })
-    })
-    .then(response => response.json())
-    .then(result => {
-        if (result.success) {
-            alert('Command sent successfully!');
-        } else {
-            alert('Failed to send command.');
-        }
-    });
-}
-
-function refreshDeviceList(devices) {
-    const deviceList = document.getElementById('deviceList');
-    deviceList.innerHTML = ""; // clear the list
-    
-    if (devices.length === 0) {
-        const li = document.createElement('li');
-        li.textContent = "No devices connected.";
-        deviceList.appendChild(li);
-    } else {
-        devices.forEach(device => {
-            const li = document.createElement('li');
-            li.textContent = device;
-            deviceList.appendChild(li);
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                message.textContent = 'Command sent successfully!';
+            } else {
+                message.textContent = data.message;
+            }
+            commandInput.value = '';
         });
+    } else {
+        message.textContent = 'Please select a device first.';
     }
 }
 
+// Fetch the devices every 1 seconds
+setInterval(fetchDevices, 1000);
